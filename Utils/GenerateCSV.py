6dbc0bd682e60eaa014e57211849667b30d84a50
yad <yadhr582855555@hotmail.com>
2019-06-23 18:08:35 +0800

    excel utils
    export all the sheets to a single csv file.

---
+++ Utils/GenerateCSV.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+
+
+# export all the sheets to a single csv file.
+# params:
+#   input_excel_path : excel file path.
+#   target_dir : It will be the same as input_excel_path when no content inputted.
+def generate_csv(input_excel_path, target_dir):
+    # path format , the file will export to the same dir as input.
+    if target_dir is None or target_dir.strip() == "":
+        target_dir = (os.path.dirname(os.path.abspath(input_excel_path)))
+    else:
+        pass
+    print("source_excel_path :" + input_excel_path)
+    print("target_dir :" + target_dir)
+
+    source_excel_pd = pd.read_excel(input_excel_path,sheet_name=None)
+
+    sheet_names_list = list(source_excel_pd.keys())
+
+    # No sheets to export
+    if sheet_names_list is None or sheet_names_list.__len__() == 0:
+        print("no sheets to export , function end")
+        return
+    for i in range(sheet_names_list.__len__()):
+        source_excel_pd[sheet_names_list[i]].to_csv(target_dir + "/" + sheet_names_list[i] + ".csv", encoding="utf-8")
